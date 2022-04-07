edd6abf911b576fba1ca8813a6ce5dd4075d1489
zipong <39720650+zipong47@users.noreply.github.com>
2022-04-07 11:27:56 +0800

    add swiper pics

--- main.js
+++ main.js
@@ -2,6 +2,23 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+// 配置网络请求
+uni.$http = $http
+// 配置网络请求根路径（如果要上线需要满足域名配置需求）
+$http.baseUrl = 'http://localhost:8080'
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
 
 Vue.config.productionTip = false
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,12 +8,36 @@
   export default {
     data() {
       return {
-
-      };
-    }
+        // 1. 轮播图的数据列表，默认为空数组
+        swiperList: [],
+      }
+    },
+    onLoad() {
+      // 2. 在小程序页面刚加载的时候，调用获取轮播图数据的方法
+      this.getSwiperList()
+    },
+    methods: {
+      // 3. 获取轮播图数据的方法
+      async getSwiperList() {
+        // 3.1 发起请求
+        const {
+          data: res
+        } = await uni.$http.get('/getSwiper')
+        // 3.2 请求失败
+        if (res.status !== 200) {
+          return uni.showToast({
+            title: '数据请求失败！',
+            duration: 1500,
+            icon: 'none',
+          })
+        }
+        // 3.3 请求成功，为 data 中的数据赋值
+        this.swiperList = res.list
+      },
+    },
   }
 </script>
 
 <style lang="scss">
-  
+
 </style>
